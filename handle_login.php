8e5b91d71ac75648f2ced740bb46de6db77f06f7
TheBeachmaster <otienoken@outlook.com>
2017-08-01 10:58:09 +0300

    Fixed Client ID

--- handle_login.php
+++ handle_login.php
@@ -7,12 +7,14 @@ $r = curl_exec($c);
 curl_close($c);
 $d = json_decode($r);
  
-if ($d->aud != 'amzn1.devportal.webapp.45606d905a5741c48885b7cd771202e1') {
+if ($d->aud != 'amzn1.application-oa2-client.0959a2d3ab2342e396cf3386a73baba8') {
   // the access token does not belong to us
   header('HTTP/1.1 404 Not Found');
   echo 'Page not found';
   exit;
 }
+//Sec e985af880376a38af738f1313682187e28b10c58b3c05e4d7a44081e1194d1b7
+//Red https://pitangui.amazon.com/api/skill/link/M2GQUV057P11YV
  
 // exchange the access token for user profile
 $c = curl_init('https://api.amazon.com/user/profile');
