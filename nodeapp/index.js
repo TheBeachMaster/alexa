51a65d08a074d8d9fd4d98924439409766efaa0f
TheBeachmaster <otienoken@outlook.com>
2017-08-29 12:43:31 +0300

    Added MQTT payments

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -1,28 +1,38 @@
 var express = require('express');
+var mqtt = require('mqtt');
+var client = mqtt.connect('mqtt://sungura1-angani-ke-host.africastalking.com:1882');
 var options = {
     sandbox: true, // true/false to use/not sandbox
     apiKey: 'e83d183e5cd04bc6c60c3a0051da5908c34a6016c45abfe4ff4cec2f88b59e89', // Use sandbox username and API key if you're using the sandbox
     username: 'sandbox', //
     format: 'json' // or xml
 };
-var AfricasTalking = require('africastalking')(options);
 var app = express();
 app.get('/', (req, res) => {
     res.status(200).send('Hello, world!').end();
 });
-app.post('/pay', (req, res) => {
-    var pay = AfricasTalking.PAYMENTS;
+var AfricasTalking = require('africastalking')(options);
+var pay = AfricasTalking.PAYMENTS;
+client.on('connect', function() {
+    client.subscribe('payments');
+});
+client.on('message', function(topic, message) {
+    var amt = parseInt(message.toString());
+    console.log(amt);
     var opts = {
         productName: 'awesomeproduct',
         phoneNumber: '+254724587654',
         currencyCode: 'KES',
-        amount: 1500
+        amount: amt
 
     };
-    pay.checkout(opts)
-        .then(console.log('Success'))
-        .catch(console.log('Error'));
+    try {
+        pay.checkout(opts);
+    } catch (error) {
+        console.log('Woops!');
+    }
 });
+
 const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
